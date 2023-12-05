822a000860c885b97956fc4b3c21e15af7ce0563
Khushiyant <khushiyant2002@gmail.com>
2023-12-06 00:47:06 +0530

    chore: add logo--ascii

--- quasar/utils/ascii.py
+++ quasar/utils/ascii.py
@@ -1,6 +1,28 @@
 from termcolor import colored
 
-art = """ 
+art = """                                                                                             
+                            nn                                  
+                            no i                                 
+                            o                                    
+                          mpo    l                               
+                      opppn       opn                           
+                    ooppopo         oppol                        
+                  oopoppoo           opopo                       
+                oopooono             oppppo                     
+                pppppomp               oppppo                    
+              opppo  p                 mppppl                   
+              oppon om      onm         ooppo                   
+              loppp no     no  on         opoo                   
+              npppono     o      no       pppo                   
+              oppo k   om         o  n  mpppo                   
+              opppo     ol       nn nm  ooppo                   
+              lppppo     oo    no  mo  ooppo                    
+                oppppn      onno   ln  oopoo                     
+                ooppppm      n   no opppoo                      
+                  opopppo       mpoppopoo                       
+                    nppppo      pppppo                          
+                        oooo    opooo                                                                  
+                                                                                                                                                                                          
   .ooooo oo oooo  oooo   .oooo.    .oooo.o  .oooo.   oooo d8b
   d88' `888  `888  `888  `P  )88b  d88(  "8 `P  )88b  `888""8P 
   888   888   888   888   .oP"888  `"Y88b.   .oP"888   888     
