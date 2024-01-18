e1293b30f9a0ea850f3b56abed35a5536cc5239c
Khushiyant <khushiyant2002@gmail.com>
2024-01-18 13:30:45 +0530

    chore: add error handling

--- quasar/algorithm/train.py
+++ quasar/algorithm/train.py
@@ -1,21 +1,25 @@
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
-import numpy as np
 import pickle
-from quasar.config import ModelType
 from typing import Optional
 import os
 from quasar.utils.logger import logger
+import click
 
 
-def train(type: str, dataset: str = None, output: Optional[str] = os.path.expanduser('~')):
-    def _trainer():
+
+def train(type: str, dataset: str = None, output:str = "model") -> None:
+    
+    dir_path = f"{os.path.dirname(os.path.realpath(__file__))}" 
+
+    def _trainer() -> None:
         df = pd.read_csv(dataset, sep=',')
         X = df.iloc[:, :-1].values
         y = df.iloc[:, -1].values
 
         model = RandomForestClassifier(
             n_estimators=100, max_depth=2, random_state=0)
+
         try:
             model.fit(X, y)
             pickle.dump(model, open(f"{output}/{type}.pkl", 'wb'))
@@ -23,11 +27,14 @@ def train(type: str, dataset: str = None, output: Optional[str] = os.path.expand
         except Exception as e:
             logger.error(f"Error while training model: {e}")
 
+    output = f"{dir_path}/{output}" if output else f"{dir_path}/model"
     if dataset:
         if not os.path.exists(dataset):
-            raise ValueError('Invalid dataset path.')
+            raise click.FileError('Invalid dataset path.')
+        _trainer()
+    else:
+        raise click.BadParameter('No dataset provided.')
 
-    _trainer()
 
 
 if __name__ == "__main__":
