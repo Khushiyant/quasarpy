800ad80b453a00cd7a6f868c622dadae71da1004
Khushiyant <khushiyant2002@gmail.com>
2024-02-25 14:58:40 +0530

    refactor: version numbering

--- quasar/__init__.py
+++ quasar/__init__.py
@@ -2,16 +2,15 @@
 command line interface.'''
 from dotenv import load_dotenv
 from logging import INFO
+from quasar.cli import cli
+from quasar.utils import logger
+from quasar._version import __version__ as _version
+__version__ = _version
 
-
-load_dotenv() # Load environment variables from .env file
-
-__version__ = '0.1.0'
+load_dotenv()  # Load environment variables from .env file
 
 
 def main():
-    from quasar.cli import cli
-    from quasar.utils import logger
 
     try:
         cli()
