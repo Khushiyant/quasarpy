cc4813718f81c6803787f15f4e489c22ab993749
Khushiyant <khushiyant2002@gmail.com>
2024-03-09 21:27:56 +0530

    chore: add pydantic dependency and refactor LLM class

--- quasar/algorithm/llm.py
+++ quasar/algorithm/llm.py
@@ -1,20 +1,29 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from quasar.utils.logger import logger
+from pydantic import BaseModel
+
+class LLMConfig(BaseModel):
+    model_name: str = "google/gemma-2b"
+    max_length: int = 100
+
 
 class LLM:
     logger = logger
-    def __init__(self, model_name: str = "google/gemma-2b") -> None:
+    def __init__(self, model_config:LLMConfig) -> None:
         """
         Initializes an instance of the LLM class.
 
         Args:
             model_name (str, optional): The name or path of the pre-trained language model. Defaults to "google/gemma-2b".
         """
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.model = AutoModelForCausalLM.from_pretrained(model_name)
+
+        self.model_config = model_config
+        self.tokenizer = AutoTokenizer.from_pretrained(model_config.model_name)
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_config.model_name)
         self.logger.info('LLM class initialized.')
 
-    def generate(self, input_text: str, max_length: int, **kwargs) -> str:
+    def generate(self, input_text: str, **kwargs) -> str:
         """
         Generates text based on the given input text.
 
@@ -27,6 +36,6 @@ class LLM:
             str: The generated text.
         """
         input_ids = self.tokenizer(
-            input_text, return_tensors="pt", max_length=max_length, kwargs=kwargs)
+            input_text, return_tensors="pt", max_length=self.model_config.max_length, kwargs=kwargs)
         outputs = self.model.generate(**input_ids)
         return self.tokenizer.decode(outputs[0])
