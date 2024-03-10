2ab207a001c2cd9789eaa7292193e10882ea8f6d
Khushiyant <khushiyant2002@gmail.com>
2024-03-10 19:26:21 +0530

    tests: add fixtures for issue with label and repository

--- quasar/tests/conftest.py
+++ quasar/tests/conftest.py
@@ -1,7 +1,16 @@
 import pytest
+from quasar.handler import Repository
 from quasar.handler import Issue
 
 
 @pytest.fixture
 def issue() -> Issue:
     return Issue(title='test', body='test')
+
+@pytest.fixture
+def issue_with_label() -> Issue:
+    return Issue(title='test', body='test', labels=['improvement'])
+
+@pytest.fixture
+def repository() -> Repository:
+    return Repository(name='test', token='test')
